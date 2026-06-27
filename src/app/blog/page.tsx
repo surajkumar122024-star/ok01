b302ee97884ca0099d16f2c2a0f4184bacc28fc9
surajkumar122024-star <surajkumar122024@gmail.com>
2026-06-27 15:10:24 +0530

    Add new blog post about adding watermarks to images

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -43,6 +43,12 @@ const blogs = [
     description: 'The ultimate cheat sheet for image dimensions on Instagram, Twitter, YouTube and more.',
     date: 'June 14, 2026',
   },
+  {
+  slug: 'add-watermark-to-image-online',
+  title: 'How to Add a Watermark to an Image Online',
+  description: 'Learn how to add text or logo watermarks to your images online. Protect your photos, artwork, and branding with this simple step-by-step guide.',
+  date: 'June 27, 2026',
+},
 ];
 
 export default function BlogPage() {
