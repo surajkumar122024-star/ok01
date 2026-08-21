7648c770a386c1a793c8629ce823ce7bf28fa20e
surajkumar122024-star <surajkumar122024@gmail.com>
2026-08-21 11:37:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 import type { Metadata } from 'next';
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
@@ -34,11 +35,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6461565201811259"
-          crossOrigin="anonymous"
-        />
+        <Script
+         async
+         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6461565201811259"
+         crossOrigin="anonymous"
+         strategy="lazyOnload"
+         />
       </head>
       <body className="font-body antialiased premium-dark-bg text-foreground selection:bg-primary/20 selection:text-primary">
         <Navbar />
