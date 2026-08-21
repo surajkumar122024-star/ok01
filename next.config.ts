23c6b8525d70fce15b17a01e8aa22eeece8c4d90
surajkumar122024-star <surajkumar122024@gmail.com>
2026-08-21 12:49:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -39,11 +39,11 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com *.googlesyndication.com *.googletagmanager.com *.google-analytics.com *.doubleclick.net",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com *.googlesyndication.com *.googletagmanager.com *.google-analytics.com *.doubleclick.net *.adtrafficquality.google",
               "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
               "font-src 'self' fonts.gstatic.com",
               "img-src 'self' data: blob: *.google.com *.googlesyndication.com *.doubleclick.net *.gstatic.com images.unsplash.com picsum.photos placehold.co",
-              "connect-src 'self' *.google.com *.googlesyndication.com *.google-analytics.com *.doubleclick.net",
+              "connect-src 'self' *.google.com *.googlesyndication.com *.google-analytics.com *.doubleclick.net *.adtrafficquality.google *.googletagmanager.com",
               "frame-src 'self' *.google.com *.googlesyndication.com *.doubleclick.net",
             ].join('; '),
           },
